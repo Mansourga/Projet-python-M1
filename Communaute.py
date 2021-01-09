5fa7330a83264c7b326f857162c3ac6a01ae0785
Mansour GAYE <gayemansour1@outlook.fr>
2021-01-09 12:59:30 +0100

    application_modifié

---
+++ Communaute.py
@@ -0,0 +1,29 @@
+import sys
+from sys import argv
+import os
+from os import path
+import pathlib
+import time
+import initi
+import time
+
+#import projet
+
+def main():
+    if len(argv)==4:
+        if argv[1]== 'init' :
+            if  path.isdir(argv[2]) == True and path.isfile(argv[3])==True:
+                ini=initi.Initialisation(argv[2],argv[3])
+                ini.initialiser(argv[2],argv[3])
+                
+        else:
+            print("pas bien")
+    if len (argv)==3:
+        import cite
+        if argv[1]=="cite":
+            cite.liste_des_auteurs_cités(argv[2])
+        else:
+            print("Veuillez entrer un d'auteur qui existe")
+if __name__== "__main__":
+    main()
+
